9e498868323aad503c4254a7406cd0cf5834d3db
Tim Graupmann <tgraupmann@gmail.com>
2018-02-22 15:27:53 -0800

    Create initial project

---
+++ Source/UE4ChromaSDKRT/UE4ChromaSDKRTGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4ChromaSDKRT.h"
+#include "UE4ChromaSDKRTGameMode.h"
+
+
+AUE4ChromaSDKRTGameMode::AUE4ChromaSDKRTGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
