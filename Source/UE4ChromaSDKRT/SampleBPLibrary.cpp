2a5c406c616aa7837a5538f0c624b4bb3a25910c
Tim Graupmann <tgraupmann@gmail.com>
2018-03-05 12:20:22 -0800

    Added hotkey sample

--- Source/UE4ChromaSDKRT/SampleBPLibrary.cpp
+++ Source/UE4ChromaSDKRT/SampleBPLibrary.cpp
@@ -31,10 +31,27 @@ void USampleBPLibrary::SampleClearComposite()
 
 void USampleBPLibrary::SampleShowHotkeys()
 {
+	TArray<TEnumAsByte<EChromaSDKKeyboardKey::Type>> keys = TArray<TEnumAsByte<EChromaSDKKeyboardKey::Type>>();
+	keys.Add(EChromaSDKKeyboardKey::KK_W);
+	keys.Add(EChromaSDKKeyboardKey::KK_A);
+	keys.Add(EChromaSDKKeyboardKey::KK_S);
+	keys.Add(EChromaSDKKeyboardKey::KK_D);
+	keys.Add(EChromaSDKKeyboardKey::KK_LOGO);
+	keys.Add(EChromaSDKKeyboardKey::KK_ESC);
 
+	FString target = "Random_Keyboard";
+	UChromaSDKPluginBPLibrary::CloseAnimationName(target);
+
+	FString source = "Fire_Keyboard";
+	UChromaSDKPluginBPLibrary::CopyKeysColorAllFramesName(source, target, keys);
+
+	UChromaSDKPluginBPLibrary::PlayAnimation(target, true);
 }
 
 void USampleBPLibrary::SampleHideHotkeys()
 {
+	FString target = "Random_Keyboard";
+	UChromaSDKPluginBPLibrary::CloseAnimationName(target);
 
+	UChromaSDKPluginBPLibrary::PlayAnimation(target, true);
 }
