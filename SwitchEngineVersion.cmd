ed87823c47ae250fcc67fcd1366621f6c8aeb3a2
Tim Graupmann <tgraupmann@gmail.com>
2018-04-25 16:03:57 -0700

    Fixed uproject reference

--- SwitchEngineVersion.cmd
+++ SwitchEngineVersion.cmd
@@ -1,4 +1,4 @@
-CALL "C:\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe" /switchversion "%CD%\UE4ChromaSDK.uproject"
+CALL "C:\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe" /switchversion "%CD%\UE4ChromaSDKRT.uproject"
 ECHO ****
 ECHO ****
 ECHO *****
