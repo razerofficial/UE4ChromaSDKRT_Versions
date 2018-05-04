bc9c782ebeff4b5c475919c68cc0233d0200414e
Tim Graupmann <tgraupmann@gmail.com>
2018-05-04 12:42:22 -0700

    Automated source packaging

--- Package_Source.cmd
+++ Package_Source.cmd
@@ -6,8 +6,12 @@ FOR /l %%i in (5, 1, 19) DO (
 COPY /Y "empty.zip" "%CD%\ChromaSDKPlugin_Source_4_%%i.zip"
 %APP% 4.%%i
 COPY /Y UE4ChromaSDKRT_4_%%i.uproject UE4ChromaSDKRT.uproject
-REM IF EXIST "%CD%\ChromaSDKPlugin_Source_4_%%i.zip" "C:\Program Files\7-Zip\7zFM.exe" "%CD%\ChromaSDKPlugin_Source_4_%%i.zip"
-"C:\Program Files\7-Zip\7zFM.exe" "%CD%\ChromaSDKPlugin_Source_4_%%i.zip"
-PAUSE
+"C:\Program Files\7-Zip\7z" a "%CD%\ChromaSDKPlugin_Source_4_%%i.zip" UE4ChromaSDKRT.uproject
+"C:\Program Files\7-Zip\7z" a "%CD%\ChromaSDKPlugin_Source_4_%%i.zip" Source
+"C:\Program Files\7-Zip\7z" a "%CD%\ChromaSDKPlugin_Source_4_%%i.zip" Plugins\ChromaSDKPlugin\ChromaSDKPlugin.uplugin
+"C:\Program Files\7-Zip\7z" a "%CD%\ChromaSDKPlugin_Source_4_%%i.zip" Plugins\ChromaSDKPlugin\Resources
+"C:\Program Files\7-Zip\7z" a "%CD%\ChromaSDKPlugin_Source_4_%%i.zip" Plugins\ChromaSDKPlugin\Source
+REM "C:\Program Files\7-Zip\7zFM.exe" "%CD%\ChromaSDKPlugin_Source_4_%%i.zip"
+REM PAUSE
 )
 PAUSE
